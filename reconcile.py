3341ac66d61db381e5de51d3dd3ac9d3453599d6
David Seddon <david.seddon@growthstreet.co.uk>
2017-03-30 09:04:04 +0100

    Yaml config proof of concept

--- reconcile.py
+++ reconcile.py
@@ -1,8 +1,14 @@
 import piecash
+import yaml
 
-with piecash.open_book("files/accounts.gnucash", readonly=False) as book:
-    # get default currency of book
-    imbalance_account = book.accounts[-1]
-    split = imbalance_account.splits[0]
-    split.account = book.accounts.get(name='Income')
-    book.save()
+if __name__ == '__main__':
+    with open("files/accounts.yaml") as config_file:
+        yaml_string = config_file.read()
+        match_config = yaml.load(yaml_string)
+
+    with piecash.open_book("files/accounts.gnucash", readonly=False) as book:
+        # get default currency of book
+        imbalance_account = book.accounts[-1]
+        split = imbalance_account.splits[0]
+        split.account = book.accounts.get(name='Income')
+        book.save()
