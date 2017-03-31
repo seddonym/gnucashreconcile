63b49954148227b73bf7def502746ef074586bd4
David Seddon <david@seddonym.me>
2017-03-31 12:29:23 +0100

    WIP

--- reconcile.py
+++ reconcile.py
@@ -1,14 +1,43 @@
 import piecash
 import yaml
 
-if __name__ == '__main__':
-    with open("files/accounts.yaml") as config_file:
-        yaml_string = config_file.read()
-        match_config = yaml.load(yaml_string)
 
-    with piecash.open_book("files/accounts.gnucash", readonly=False) as book:
-        # get default currency of book
-        imbalance_account = book.accounts[-1]
-        split = imbalance_account.splits[0]
-        split.account = book.accounts.get(name='Income')
-        book.save()
+class Config:
+    """Reads and stores configuration from a YAML file.
+    """
+    def __init__(self, filename):
+        self._load_from_file(filename)
+
+    def _load_from_file(self, filename):
+        """Parses the supplied yaml filename.
+        """
+        with open("files/accounts.yaml") as config_file:
+            yaml_string = config_file.read()
+            self._config_dict = yaml.load(yaml_string)
+
+
+class Book:
+    """The entire account GnuCash book.
+    """
+    def __init__(self, filename):
+        self._load_book_from_file(filename)
+
+    def _load_from_file(self, filename):
+        """Opens and initializes the Gnucash file.
+        """
+        with piecash.open_book(filename, readonly=False) as piecash_book:
+            self._piecash_book = piecash_book
+
+
+if __name__ == '__main__':
+    # TODO make these command line arguments
+    CONFIG_FILENAME = 'files/accounts.yaml'
+    ACCOUNTS_FILENAME = 'files/accounts.gnucash'
+
+    config = Config(filename=CONFIG_FILENAME)
+    book = Book(filename=ACCOUNTS_FILENAME)
+
+            imbalance_account = book.accounts[-1]
+            split = imbalance_account.splits[0]
+            split.account = book.accounts.get(name='Income')
+            book.save()
