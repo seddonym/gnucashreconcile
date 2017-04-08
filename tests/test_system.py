198b63d8b2f979f633caefe59d9d9794b9f4b4d2
David Seddon <david@seddonym.me>
2017-04-08 17:53:07 +0100

    Added stub test

---
+++ tests/test_system.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+class TestSystem(TestCase):
+    def test_system_success(self):
+        """Test a successful run of the entire command.
+        """
+        self.assertTrue(False)
